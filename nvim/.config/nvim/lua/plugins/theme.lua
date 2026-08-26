920a2780b119c37f5fe59c13d1aedb8e5a612210
Ty Richards <me@tyrichards.com>
2026-08-26 12:06:34 -0500

    Fix stowed Neovim theme loader

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,1 +1,2 @@
-../../../../.local/state/omarchy/current/theme/neovim.lua
+-- Load the active Omarchy theme without a location-sensitive symlink.
+return dofile(vim.fn.expand("~/.local/state/omarchy/current/theme/neovim.lua"))
